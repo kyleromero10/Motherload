e6d15aeda69858d1c5cc4b530a36211a2422aebd
Robert Grant <112195519+RGrant3734@users.noreply.github.com>
2026-02-20 12:31:08 -0500

    Base Gunpowder and Small Explosive
    Added gunpowder and small explosive that trigger eachothers activation

---
+++ Scripts/SmallExplosive.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class SmallExplosive : StaticBody2D
+{
+    [Export]
+    public Area2D explosionRadius;
+    public bool lit = false;
+
+    [Export]
+    public CompressedTexture2D litTexture;
+    [Export]
+    public CompressedTexture2D explodedTexture;
+
+    public async void Lit()
+    {
+        // Mark that it has been lit and change its texture
+        lit = true;
+        GetChild<Sprite2D>(1).Texture = litTexture;
+        // wait to explode
+        await ToSignal(GetTree().CreateTimer(2f), SceneTreeTimer.SignalName.Timeout);
+        Explode();
+        GetChild<Sprite2D>(1).Texture = explodedTexture;
+    }
+
+    public async void Explode()
+    {
+        // checks each node in its radius and lights the specific ones
+        foreach(StaticBody2D node in explosionRadius.GetOverlappingBodies())
+        {
+            // Gunpowder trails
+            if(node is Gunpowder gunpowder)
+            {
+                if(!gunpowder.lit)
+                    gunpowder.Lit();
+            }
+            // small explosives
+            else if(node is SmallExplosive smallExplosive)
+            {
+                if(!smallExplosive.lit)
+                    smallExplosive.Lit();
+            }
+        }
+        // deletes itself because it was causing lag by staying in the world
+        QueueFree();
+    }
+}
