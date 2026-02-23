fccf4b44469abb4164cc4c8e8bb65aec98629503
Bassil Ghareeb <87038632+leChickenfish@users.noreply.github.com>
2026-02-22 19:43:19 -0500

    Fixed health display bug

--- Scripts/GameMaster.cs
+++ Scripts/GameMaster.cs
@@ -37,7 +37,6 @@ public partial class GameMaster : Node
 	public void EndLevel()
 	{
 		// changes the scene to the win scene
-		UI.GetNode<Label>("SupportHealth").Text = "Support Health: " + 0;
 		winScreen.GetNode<Label>("Label2").Text = "Gold Collected: " + goldCollected;
 		winScreen.Visible = true;
 		
@@ -49,6 +48,7 @@ public partial class GameMaster : Node
 	public void FailedLevel()
 	{
 		// changes the scene to the failed scene
+		UI.GetNode<Label>("SupportHealth").Text = "Support Health: " + 0;
 		loseScreen.Visible = true;
 		GD.Print("Level Failed");
 		
